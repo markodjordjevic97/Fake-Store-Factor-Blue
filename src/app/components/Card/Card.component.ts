1b567d6dd32aca53bf3fce4baf6b123e074b606f
MarkoJoker <djordjevicit97@gmail.com>
2022-05-06 01:38:39 +0200

    Implement Card Component

--- src/app/components/Card/Card.component.ts
+++ src/app/components/Card/Card.component.ts
@@ -1,11 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
+export interface Card {
+  title: string,
+  description: string,
+  image: string
+}
 @Component({
-  selector: 'app-Card',
+  selector: 'app-card',
   templateUrl: './Card.component.html',
   styleUrls: ['./Card.component.scss']
 })
 export class CardComponent implements OnInit {
+
+  @Input() data!: Card;
 
   constructor() { }
 
