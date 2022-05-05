9a61c66f9a3a2a68ce8774a0335bf9d2af3b9666
MarkoJoker <djordjevicit97@gmail.com>
2022-05-06 00:15:09 +0200

    Create Card Component

---
+++ src/app/components/Card/Card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Card',
+  templateUrl: './Card.component.html',
+  styleUrls: ['./Card.component.scss']
+})
+export class CardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { NgxPaginationModule } from 'ngx-pagination';
+
+// Components
 import { StoresListComponent } from '../components/StoresList/StoresList.component';
 import { StoreItemComponent } from '../components/StoreItem/StoreItem.component';
+import { RouterModule } from '@angular/router';
+import { CardComponent } from '../components/Card/Card.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    HttpClientModule
-  ],
-  declarations: [StoresListComponent, StoreItemComponent]
+  imports: [CommonModule, HttpClientModule, NgxPaginationModule, RouterModule],
+  declarations: [StoresListComponent, StoreItemComponent, CardComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
