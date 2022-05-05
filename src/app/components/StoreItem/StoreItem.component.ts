98ab924a6a51e575baeda43c9e3a407c011cb382
MarkoJoker <djordjevicit97@gmail.com>
2022-05-06 01:38:58 +0200

    Design Store item and Stores List

--- src/app/components/StoreItem/StoreItem.component.ts
+++ src/app/components/StoreItem/StoreItem.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { untilDestroyed } from 'ngx-take-until-destroy';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Store } from 'src/app/shared/store/store.model';
+import { StoresQuery } from 'src/app/shared/store/store.query';
 
 @Component({
   selector: 'app-StoreItem',
   templateUrl: './StoreItem.component.html',
   styleUrls: ['./StoreItem.component.scss']
 })
-export class StoreItemComponent implements OnInit {
+export class StoreItemComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public store!: Store;
+
+  constructor(private route: ActivatedRoute, private storesQuery: StoresQuery) { }
 
   ngOnInit() {
+    this.getStore();
   }
 
+  private getStore() {
+    this.route.paramMap.pipe(untilDestroyed(this)).subscribe(param => {
+      const id: string = param.get('id')!;
+      this.store = this.storesQuery.getEntity(parseInt(id))!;
+    })
+  }
+
+  ngOnDestroy(): void {}
 }
--- src/app/components/StoresList/StoresList.component.ts
+++ src/app/components/StoresList/StoresList.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { PaginatePipeArgs } from 'ngx-pagination/dist/paginate.pipe';
 import { untilDestroyed } from 'ngx-take-until-destroy';
@@ -10,9 +10,14 @@ import { StoresQuery } from 'src/app/shared/store/store.query';
   selector: 'app-StoresList',
   templateUrl: './StoresList.component.html',
   styleUrls: ['./StoresList.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class StoresListComponent implements OnInit, OnDestroy {
   public stores!: any[];
+  private originalStoresList!: any[];
+
+  public cartAmount: number = 0;
+
   public tableHeader: string[] = [
     'Id',
     'Title',
@@ -38,12 +43,26 @@ export class StoresListComponent implements OnInit, OnDestroy {
     this.changePaginationPages();
   }
 
-  public search(event: any) {}
-
   private getStores() {
     this.stores = this.storesQuery
       .getAll()
-      .map((item) => ({ ...item, addToCart: 'Add' }));
+      .map((item) => ({
+        ...item,
+        addToCart: 'Add',
+        removeFromCart: 'Delete',
+        isAddedToCart: false,
+      }));
+    this.originalStoresList = this.stores;
+  }
+
+  public search(event: any) {
+    if (event.target.value) {
+      this.stores = this.originalStoresList.filter((item) =>
+        item.title.toLowerCase().includes(event.target.value.toLowerCase())
+      );
+    } else {
+      this.stores = this.originalStoresList;
+    }
   }
 
   private changePaginationPages() {
@@ -61,6 +80,16 @@ export class StoresListComponent implements OnInit, OnDestroy {
 
   public pageChange(newPage: number) {
     this.router.navigate([''], { queryParams: { page: newPage } });
+  }
+
+  public addToCart(store: any) {
+    store.isAddedToCart = !store.isAddedToCart;
+    if(store.isAddedToCart) {
+      this.cartAmount += store.price
+    }
+    else {
+      this.cartAmount -= store.price
+    }
   }
 
   public identity(index: number, store: Store) {
