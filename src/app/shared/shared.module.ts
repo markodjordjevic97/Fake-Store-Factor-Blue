aa09067bbdf7d02bf25d64ffc440d76d48946203
MarkoJoker <djordjevicit97@gmail.com>
2022-05-06 01:39:15 +0200

    Import and install some libraries

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,15 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgxPaginationModule } from 'ngx-pagination';
+import { RouterModule } from '@angular/router';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+
 
 // Components
 import { StoresListComponent } from '../components/StoresList/StoresList.component';
 import { StoreItemComponent } from '../components/StoreItem/StoreItem.component';
-import { RouterModule } from '@angular/router';
 import { CardComponent } from '../components/Card/Card.component';
-
+// Store
+import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
+import { environment } from 'src/environments/environment';
 @NgModule({
-  imports: [CommonModule, HttpClientModule, NgxPaginationModule, RouterModule],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    NgxPaginationModule,
+    RouterModule,
+    environment.production ? [] : AkitaNgDevtools.forRoot(),
+    AngularSvgIconModule.forRoot()
+  ],
   declarations: [StoresListComponent, StoreItemComponent, CardComponent],
 })
 export class SharedModule {}
