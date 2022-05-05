4e8d75208d89885fa19f12f88c6fad2fb49d5e51
MarkoJoker <djordjevicit97@gmail.com>
2022-05-05 23:35:36 +0200

    - Created components: StoreItem, StoreList

---
+++ src/app/components/StoreItem/StoreItem.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-StoreItem',
+  templateUrl: './StoreItem.component.html',
+  styleUrls: ['./StoreItem.component.scss']
+})
+export class StoreItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/StoresList/StoresList.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-StoresList',
+  templateUrl: './StoresList.component.html',
+  styleUrls: ['./StoresList.component.scss']
+})
+export class StoresListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
